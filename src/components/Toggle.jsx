753d027134cd47f39e21179d8af6a87ed428ce47
ruhulaminbd93 <ruhulaminbd93@gmail.com>
2023-02-03 03:00:35 +0600

    conditional rendering with class and functional rendering

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
 
+function Bangladesh() {
+  return (
+    <div>
+      <h1>Bangladesh</h1>
+      <p>Bangladesh ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, esse quo minus officiis nulla soluta architecto non molestias recusandae, beatae libero odit porro perferendis explicabo quam facilis voluptatibus sapiente eos?</p>
+    </div>
+  );
+}
+
+function India() {
+  return (
+    <div>
+      <h1>India</h1>
+      <p>India ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, esse quo minus officiis nulla soluta architecto non molestias recusandae, beatae libero odit porro perferendis explicabo quam facilis voluptatibus sapiente eos?</p>
+    </div>
+  );
+}
+
 class Toggle extends Component {
   constructor() {
     super();
-    this.state = { show: true };
+    this.state = { country: 'BD' };
   }
 
   render() {
     const clickHandler = () => {
-      this.setState((state) => ({ show: !state.show }));
+      this.setState((state) => ({ country: state.country === 'BD' ? 'IN' : 'BD' }));
     };
-    const { show } = this.state;
-
+    const { country } = this.state;
     return (
       <div>
-        <button onClick={clickHandler} type="button">{show ? 'Hide' : 'Show'}</button>
-        {show && <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae beatae autem accusamus aut consequatur veniam earum libero quis maiores itaque voluptatem, optio labore? Tenetur ratione quisquam soluta natus consectetur repudiandae.</p>}
+        <button onClick={clickHandler} type="button">{country === 'BD' ? 'Show India' : 'Show Bangladesh'}</button>
+        {country === 'BD' ? <Bangladesh /> : <India /> }
       </div>
     );
   }
