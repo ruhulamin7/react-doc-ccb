e143f436ebcb27e8da59f0fe6dbcc2defcb1841f
ruhulaminbd93 <ruhulaminbd93@gmail.com>
2023-02-03 03:21:07 +0600

    lists and keys

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -29,10 +29,16 @@ class Toggle extends Component {
       this.setState((state) => ({ country: state.country === 'BD' ? 'IN' : 'BD' }));
     };
     const { country } = this.state;
+
+    // array of fruits
+    const fruits = ['Mango', 'Orange', 'Apple', 'Banana'];
     return (
       <div>
         <button onClick={clickHandler} type="button">{country === 'BD' ? 'Show India' : 'Show Bangladesh'}</button>
         {country === 'BD' ? <Bangladesh /> : <India /> }
+        <ul>
+          {fruits.map((fruit) => <li key={Math.floor(Math.random() * 100) + 1}>{fruit}</li>)}
+        </ul>
       </div>
     );
   }
